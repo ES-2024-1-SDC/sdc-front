117cca0323758fe8bc302b2640ecc3be35340f16
bcosta19 <brpassos@id.uff.br>
2024-05-24 00:28:13 -0300

    Começando a implementar parte do login no sistema

--- sdc_front/lib/pages/login.dart
+++ sdc_front/lib/pages/login.dart
@@ -1,7 +1,9 @@
 // import 'dart:ffi';
 
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'inicio.dart';
 
 enum LoginStatus { notSignIn, signIn }
@@ -24,6 +26,23 @@ class _LoginState extends State<Login> {
   late String sessionToken;
   late int userId;
 
+  final _storage = FlutterSecureStorage();
+
+  Future<void> _login() async {
+
+    // Lógica da autenticação com o servidor usando http e obter o token de autenticação
+
+    await _storage.write(key: 'auth_token', value: sessionToken);
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user_username', _username!);
+
+    Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+            builder: (context) => Inicio(title: 'Caronas Uff')));
+  }
+
   //USER_ID E SESSION TOKEN SERÃO RETORNADOS PELO SERVIDOR
 
   final _formKey = GlobalKey<FormState>();
@@ -33,99 +52,124 @@ class _LoginState extends State<Login> {
     switch (_loginStatus) {
       case LoginStatus.notSignIn:
         return Scaffold(
-          
           // appBar: AppBar(
           //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           //   title: Text("Login / Cadastro"),
           // ),
           body: Container(
-            padding: EdgeInsets.only(left: 40,right:40,top:40)
-            ,decoration: BoxDecoration( 
-              gradient: LinearGradient( 
-      colors: [Colors.white, Colors.indigo.shade50], // Define your gradient colors here
-    ), ),
+            padding: EdgeInsets.only(left: 40, right: 40, top: 40),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white,
+                  Colors.indigo.shade50
+                ], // Define your gradient colors here
+              ),
+            ),
             child: ListView(
-              children: [ 
-                Image.asset('assets/caronaUffLogo.png',alignment: AlignmentDirectional.topCenter,)
-                ,Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              Row(children:[
-                                Icon(Icons.account_circle_outlined)
-                                ,Text('Nome de Usuário')
-                              ]),
-                              Padding(
-                                  padding: EdgeInsets.all(5),
-                                  child: TextFormField(
-                                      onSaved: ((newValue) => _username = newValue),
-                                      decoration: InputDecoration(
-                                        labelText: "Username",
-                                        border: OutlineInputBorder(),
-                                      ))),
-                              Row(children:[
-                                Icon(Icons.lock_outline)
-                                ,Text('Senha')
-                                
-                              ])
-                              ,Padding(
+              children: [
+                Image.asset(
+                  'assets/caronaUffLogo.png',
+                  alignment: AlignmentDirectional.topCenter,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            Row(children: [
+                              Icon(Icons.account_circle_outlined),
+                              Text('Nome de Usuário')
+                            ]),
+                            Padding(
                                 padding: EdgeInsets.all(5),
                                 child: TextFormField(
-                                  obscureText: true,
-                                  onSaved: (newValue) => _password = newValue,
-                                  decoration: InputDecoration(
-                                    labelText: "Password",
-                                    border: OutlineInputBorder(),
-                                  ),
+                                    onSaved: ((newValue) =>
+                                        _username = newValue),
+                                    decoration: InputDecoration(
+                                      labelText: "Username",
+                                      border: OutlineInputBorder(),
+                                    ))),
+                            Row(children: [
+                              Icon(Icons.lock_outline),
+                              Text('Senha')
+                            ]),
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: TextFormField(
+                                obscureText: true,
+                                onSaved: (newValue) => _password = newValue,
+                                decoration: InputDecoration(
+                                  labelText: "Password",
+                                  border: OutlineInputBorder(),
                                 ),
-                              )
-                            ],
-                          )),
-                           Container(
-                            alignment: Alignment.centerRight,
-                            child: TextButton(onPressed: (){}, child: Text('Esqueceu sua Senha?'))
+                              ),
                             )
-                      ,Container(
-                        height:60
-                        ,width:double.infinity
-                        // ,decoration: BoxDecoration(color: Colors.indigo.shade400)
-                        ,child: ElevatedButton(
-                            onPressed: _submit,
-                            child: Text("Login"),
-                            style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.indigo.shade400,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(0.0))),
-                        ),
-                      )
-                      ,SizedBox(height: 20,)
-                      ,Container(height:60,width:double.infinity,child: ElevatedButton(onPressed: (){}, child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center
-                        ,children: [
-                          Text('Login com Google'),
-                          Icon(Icons.g_mobiledata)
-                        ],
-                      ),style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo.shade400,foregroundColor: Colors.white,shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(0.0))),))
-                    ],
-                                  ),
-                    SizedBox(height:20),
-                    TextButton(child:Text('Cadastrar'),onPressed: (){},)
+                          ],
+                        )),
+                    Container(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text('Esqueceu sua Senha?'))),
+                    Container(
+                      height: 60,
+                      width: double.infinity
+                      // ,decoration: BoxDecoration(color: Colors.indigo.shade400)
+                      ,
+                      child: ElevatedButton(
+                        onPressed: _submit,
+                        child: Text("Login"),
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.indigo.shade400,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0.0))),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                        height: 60,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Login com Google'),
+                              Icon(Icons.g_mobiledata)
+                            ],
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.indigo.shade400,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0.0))),
+                        ))
                   ],
                 ),
+                SizedBox(height: 20),
+                TextButton(
+                  child: Text('Cadastrar'),
+                  onPressed: () {},
+                )
+              ],
             ),
+          ),
         );
-            case LoginStatus.signIn:
-                return Login();
-
+      case LoginStatus.signIn:
+        return Login();
     }
   }
 
   void _submit() {
-    Navigator.push(context,MaterialPageRoute<void>(builder: (context)=>Inicio(title:'Caronas Uff')));
+    Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+            builder: (context) => Inicio(title: 'Caronas Uff')));
   }
 }
