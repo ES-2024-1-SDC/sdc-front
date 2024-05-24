989a34231acd51d893e5cbc4b323f4bf00b0aced
bcosta19 <brpassos@id.uff.br>
2024-05-24 10:02:46 -0300

    Teste simples verificando se tem um widget com texto cadastrar

--- sdc_front/test/widget_test.dart
+++ sdc_front/test/widget_test.dart
@@ -9,22 +9,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sdc_front/main.dart';
+import 'package:sdc_front/pages/login.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const CaronaUff());
+  testWidgets('Teste text cadastrar', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: const Login(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final cadastrarFinder = find.text("Cadastrar");
+    expect(cadastrarFinder, findsOneWidget);
   });
 }
